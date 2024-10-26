d502e2b0c0fec5fb4a9dc6ce73b12db584946123
geisabitt <ageisislane@gmail.com>
2024-10-26 00:53:37 -0300

    feat: prisma service and route create user

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,11 +1,26 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { PrismaService } from 'src/database/PrismaService';
 
 @Injectable()
 export class UsersService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+
+  constructor(private prismaService: PrismaService) { }
+  
+  async create(data: CreateUserDto) {
+    const userExists = await this.prismaService.user.findFirst({
+      where: {
+        email: data.email,
+      }
+    })
+    if (userExists) {
+      throw new Error('Já existe um usuario com esse email')
+    }
+    const user = await this.prismaService.user.create({
+      data,
+    })
+    return {user};
   }
 
   findAll() {
